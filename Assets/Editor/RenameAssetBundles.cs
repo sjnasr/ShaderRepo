9a47e4519f877861dc05b7becf75721ced945f66
Sara Nasr <snasr@gsngames.com>
2016-08-11 13:55:41 -0700

    Pushing project

---
+++ Assets/Editor/RenameAssetBundles.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using System.IO;
+using UnityEngine;
+
+public class RenameAssetBundles
+{
+	[MenuItem("Assets/Rename AssetBundles")]
+	static void renameAssetBundles(){
+		string[] files = Directory.GetFiles("Assets/StreamingAssets");
+
+		foreach (var a in files){
+			if (!a.Contains(".")){
+				File.Move(a, a + ".unity3d");
+			}
+		}
+	}
+}
