ae19dc16dd8bf723fed63bfa21a84cf03a1de392
Sara Nasr <snasr@gsngames.com>
2016-08-12 14:35:36 -0700

    Added a button that will download the bundle and add it to the scene.  The scene can be seen in the game also.

---
+++ Assets/Scripts/BundleLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BundleLoader : MonoBehaviour {
+
+	public string bundleUrl;
+
+	AssetBundle bundle;
+
+	IEnumerator Start(){
+		WWW www = new WWW (bundleUrl);
+		yield return www;
+		if(www.error != null){
+			throw new System.Exception("There's an error " + www.error);
+		}
+
+		bundle = www.assetBundle;
+	}
+
+	public void Spawn(string assetName){
+		if (assetName == "") {
+			Instantiate (bundle.mainAsset);
+		} else {
+			Instantiate (bundle.LoadAsset(assetName));
+		}
+	}
+
+}
