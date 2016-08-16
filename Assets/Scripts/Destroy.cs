530e351409ed01690bfc953b2c548c1681e35f35
Sara Nasr <snasr@gsngames.com>
2016-08-16 10:50:07 -0700

    Added shapes, scripts and such

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space))
+			Destroy (gameObject);
+	}
+}
